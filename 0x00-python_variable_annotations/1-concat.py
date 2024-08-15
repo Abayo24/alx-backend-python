2f896aa023205ec2f4aa4d84347f4bb21939aef3
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-15 13:19:17 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type annotated function that takes two strings and concats them
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function hthat takes two strings and concats them
+    """
+    return str1 + str2
