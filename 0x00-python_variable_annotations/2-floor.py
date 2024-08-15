af4df80e48441162cbf427049f3cecbeb41237f7
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-15 13:29:01 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor which takes a float
+and returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """function that returns floor of a number"""
+    return math.floor(n)
