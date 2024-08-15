bde732d5e1fda4f0846bdc7a1114a9d9a7123173
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-15 13:34:39 +0300

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type annotated function that takes afloat and returns the string of it
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Functions that turns a float to a string
+    """
+    return str(n)
