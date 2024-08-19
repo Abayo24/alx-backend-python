e610079aae260ce1c2bdd4c57da963dec7c15a9c
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-19 14:36:02 +0300

    executablr

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,11 +11,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List:
     """return the list of all the delays"""
-    delays = []
-    for delay in await asyncio.gather(*(wait_random(max_delay) for _ in range(n))):
-        # Insert the delay into the sorted position
-        i = 0
-        while i < len(delays) and delays[i] < delay:
-            i += 1
-        delays.insert(i, delay)
-    return delays
+    result = [await wait_random(max_delay) for _ in range(n)]
+    return result
