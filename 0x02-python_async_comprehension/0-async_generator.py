c0757d9c651fa1102a6c138ae74d50b9d091c17f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-20 14:03:31 +0300

    async gen

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,5 +13,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     wait 1 second, then yield a random number
     """
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
