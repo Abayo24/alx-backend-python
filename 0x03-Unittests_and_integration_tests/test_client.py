13881a66e1babe4dc55f500ec8d2c069338d3fac
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-14 16:17:55 +0300

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,3 +27,17 @@ class TestGithubOrgClient(unittest.TestCase):
             f"https://api.github.com/orgs/{org_name}")
 
         self.assertEqual(result, expected_response)
+        
+    @patch('client.GithubOrgClient.org', new_callable=property)
+    def test_public_repos_url(self, mock_org):
+        """Test that _public_repos_url returns the correct URL based on the org payload"""
+
+        mock_org.return_value = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+
+        client = GithubOrgClient("google")
+
+        result = client._public_repos_url
+
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
