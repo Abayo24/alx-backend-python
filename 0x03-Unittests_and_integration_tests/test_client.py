97f2b1ed66d271e8dcb9c6ac73327c32accf627f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-17 11:42:14 +0300

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """TestGithubOrgClient class to test GithubOrgClient methods"""
 import unittest
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch, PropertyMock, MagicMock
 from parameterized import parameterized
 from client import GithubOrgClient
-from utils import get_json
+from typing import Dict, Tuple, Any
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,13 +15,16 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"})
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, expected_response, mock_get_json):
+    def test_org(self,
+                 org_name: str,
+                 expected_response: Dict[str, str],
+                 mock_get_json: MagicMock) -> None:
         """Test that GithubOrgClient.org returns the correct value"""
         mock_get_json.return_value = expected_response
 
         client = GithubOrgClient(org_name)
 
-        result = client.org
+        result: Dict[str, str] = client.org
 
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
@@ -29,7 +32,7 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected_response)
 
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
-    def test_public_repos_url(self, mock_org):
+    def test_public_repos_url(self, mock_org: PropertyMock) -> None:
         """Test that _public_repos_url returns the
         correct URL based on the org payload"""
 
